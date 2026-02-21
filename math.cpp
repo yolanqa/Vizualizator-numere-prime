0fd0fd5e3244d6e4dbffbb6e4100ae213b6dd133
yolanqa <yolanda.stefanescu@yahoo.com>
2026-02-21 02:44:47 +0200

    adaug clearScreen, sleepMilisec si drawGrid

--- math.cpp
+++ math.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <chrono>
+#include <thread>
 
 
 using namespace std;
@@ -14,7 +16,7 @@ vector<bool> math(int n){
     math[1]=false;
 
     for(int i=2; i<=sqrt(n); i++){
-        if(math[i]=true){
+        if(math[i]==true){
             for( int multiplu=i*i; multiplu<=n; multiplu+=i){
                 math[multiplu]=false;
             }
@@ -23,4 +25,50 @@ vector<bool> math(int n){
     }
     return math;
     
+}
+
+void clearScreen(){
+    cout<<"\033[2J\033[H";
+    cout.flush();
+}
+
+
+
+void sleepMilisec(int ms){
+    this_thread::sleep_for(chrono::milliseconds(ms));
+}
+
+
+void drawGrid(vector<bool> math, int n, int eliminat){
+    for(int i=2; i<=n;i++){
+        if (i==eliminat){
+            cout<<"\033[33m"<<i<<"\033[0m";
+
+        }
+
+        else if (math[i]==true){
+            cout<<"\033[31m"<<i<<"\033[0m";
+
+        }
+
+        else if (math[i]==false){
+            cout<<"\033[36m"<<i<<"\033[0m";
+
+        }
+
+        if(i%20==0){
+            cout<<"\n";
+        }
+
+
+    }
+}
+
+
+//implementare main
+int main(){
+    cin>>n;
+
+    vector<bool> 
+
 }
